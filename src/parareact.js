e6a0bdc0a5d130099fe09dd3dad21907f9112e5d
Ozgur Tanriverdi <ozgur.tanriverdi94@gmail.com>
2021-07-29 15:46:35 +0300

    element

---
+++ src/parareact.js
@@ -0,0 +1,25 @@
+const Parareact = {};
+
+Parareact.createElement = function (type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(child =>
+        typeof child === 'object' ? child : createTextElement(child),
+      ),
+    },
+  };
+};
+
+function createTextElement(text) {
+  return {
+    type: 'TEXT_ELEMENT',
+    props: {
+      nodeValue: text,
+      chilren: [],
+    },
+  };
+}
+
+export default Parareact;
